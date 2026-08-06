eba187e56e13ba69b8984942233e04f014c9fb38
Tamiron <tdrozdikova@gmail.com>
2026-08-06 14:36:27 +0300

    create calculate logics

---
+++ calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    @staticmethod
+    def summa(a: int, b: int):
+        return a + b
+
+    @staticmethod
+    def minus(a: int, b: int, reverse: bool = False):
+        if reverse:
+            a, b = b, a
+        return a - b
+
+    @staticmethod
+    def multiplication(a: int, b: int):
+        return a * b
+
+    @staticmethod
+    def division(a: int, b: int, reverse: bool = False):
+        if reverse:
+            a, b = b, a
+        return a / b
