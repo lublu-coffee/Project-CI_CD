f0b35288ba1d6c8d0b4e5af6a0ed64181f6f29b0
Tamiron <tdrozdikova@gmail.com>
2026-08-06 14:37:04 +0300

    add testing app + new workflow

---
+++ tests/test_calculator.py
@@ -0,0 +1,21 @@
+import pytest
+
+from calculator import Calculator
+
+
+class TestCalculate:
+    calc = Calculator()
+
+    def test_summa(self):
+        assert self.calc.summa(3, 0) == 3
+        assert self.calc.summa(3, 3) == 6
+        assert self.calc.summa(3, -3) == 0
+        assert self.calc.summa(-3, -3) == -6
+        assert self.calc.summa(2, 3) == 5
+
+    def test_minus(self):
+        assert self.calc.minus(3, 0) == 3
+        assert self.calc.minus(3, 3) == 0
+        assert self.calc.minus(3, -3) == 6
+        assert self.calc.minus(-3, -3) == 0
+        assert self.calc.minus(2, 3) == -1
